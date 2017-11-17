16ad5964f84b75e2e00f0588a8c26b0415f611f7
blaise <blaise.calaycay@tulip.io>
2017-11-17 11:18:35 -0500

    Added route for single user with dummy data

--- main/main.go
+++ main/main.go
@@ -20,34 +20,72 @@ var name string = "all"
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
   params := mux.Vars(r)
-  name := params["name"]
+  name = params["name"]
   fmt.Println(name)
   jsonData := `
-    [{"id": "1",
-    "slackName": "Ben Cooke",
-    "name": "Ben Cooke",
-    "url": "ben.cooke"},
-    {"id": "2",
-    "slackName": "ben.hackett",
-    "name": "Ben Hackett",
-    "url": "ben.hackett"}
-    ]
+    [{
+      "id": "1",
+      "slackName": "Ben Cooke",
+      "name": "Ben Cooke",
+      "url": "ben.cooke"},
+      {"id": "2",
+      "slackName": "ben.hackett",
+      "name": "Ben Hackett",
+      "url": "ben.hackett",
+    }]
   `
   var users []map[string]interface{}
   err := json.Unmarshal([]byte(jsonData), &users)
   if err != nil {
-    fmt.Println(err)
+    log.Fatal(err)
   }
   w.Header().Set("Content-Type", "application/json")
   json.NewEncoder(w).Encode(users)
 
 }
 
+func GetUser(w http.ResponseWriter, r *http.Request) {
+  params := mux.Vars(r)
+  name = params["name"]
+
+  jsonData := `
+    {
+      "id": "1",
+      "slackName": "Ben Cooke",
+      "name": "Ben Cooke",
+      "url": "ben.cooke",
+      "github": "https://github.com/bencookie95",
+      "gitlab": "",
+      "languages": ["Java"],
+      "repos": [
+        {
+          "name": "AndroidRestService",
+          "link": "https://github.com/BenCookie95/AndroidRestService",
+          "languages": ["Java"]
+        },
+        {
+          "name": "Blackjack",
+          "link": "https://github.com/BenCookie95/Blackjack",
+          "languages": ["Java"]
+        }
+      ]
+    }
+  `
+  var user map[string]interface{}
+  err := json.Unmarshal([]byte(jsonData), &user)
+  if err != nil {
+    log.Fatal(err)
+  }
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(user)
+
+}
+
+
 func main() {
   router := mux.NewRouter()
   router.HandleFunc("/users/{name}", GetUsers).Methods("GET")
-  // router.HandleFunc("/user/{name}", GetUser).Methods("GET")
-
-
+  router.HandleFunc("/user/{name}", GetUser).Methods("GET")
+  log.Println("Listening on port 8000")
   log.Fatal(http.ListenAndServe(":8000", router))
 }
